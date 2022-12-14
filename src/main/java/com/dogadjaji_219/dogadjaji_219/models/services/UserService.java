c08a52177481aa057d63725045a14ab1351d3384
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2022-12-14 17:10:10 +0100

    update user service

--- src/main/java/com/dogadjaji_219/dogadjaji_219/models/services/UserService.java
+++ src/main/java/com/dogadjaji_219/dogadjaji_219/models/services/UserService.java
@@ -2,9 +2,12 @@ package com.dogadjaji_219.dogadjaji_219.models.services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.dogadjaji_219.dogadjaji_219.models.entities.Role;
 import com.dogadjaji_219.dogadjaji_219.models.entities.User;
 
+@Service
 public interface UserService {
     User saveUser(User user);
     Role saveRole(Role role);
