754d81f6473d621649e6145a8e449a8d734b121d
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2022-12-14 17:40:10 +0100

    update

--- src/main/java/com/controllers/UserController.java
+++ src/main/java/com/controllers/UserController.java
@@ -1,5 +1,25 @@
 package com.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dogadjaji_219.dogadjaji_219.models.entities.User;
+import com.dogadjaji_219.dogadjaji_219.models.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController 
+@RequestMapping ("/api")
+@RequiredArgsConstructor
 public class UserController {
-    
+    private final UserService _UserService;
+
+    @GetMapping("/usres")
+    public ResponseEntity<List<User>>getUsers(){
+        return ResponseEntity.ok().body(_UserService.getUsers());
+    }
 }
