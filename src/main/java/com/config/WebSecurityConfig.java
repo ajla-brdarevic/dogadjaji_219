ecfb33db47b55287a20513138e58206d805c7c84
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2022-12-15 18:09:15 +0100

    update security

--- src/main/java/com/config/WebSecurityConfig.java
+++ src/main/java/com/config/WebSecurityConfig.java
@@ -1,8 +1,18 @@
 package com.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
-    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf->csrf.disable()).securityMatcher("/api/**").authorizeHttpRequests(auth->{
+            auth.requestMatchers("/users/**").hasRole("user");
+            auth.requestMatchers("/admin/**").hasRole("admin");
+            auth.requestMatchers("/api/users/**")
+        });
+    }
 }
