a825d9afe5abe2170022e898795733543e22be59
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2022-12-15 15:50:06 +0100

    role update

--- src/main/java/com/dogadjaji_219/dogadjaji_219/models/entities/Role.java
+++ src/main/java/com/dogadjaji_219/dogadjaji_219/models/entities/Role.java
@@ -4,15 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity @Data @NoArgsConstructor @AllArgsConstructor 
+@Entity @Data @Getter @Setter @NoArgsConstructor @AllArgsConstructor 
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String name;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @Size(max=255)
+    private String name;
 }
